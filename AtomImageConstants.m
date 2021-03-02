99616af25db1b22195145a814159658de1938ee8
Ryan Thomas <ryan.j.thomas1@gmail.com>
2021-03-02 14:05:22 +1100

    Changed Nsat calculation
    Realized that I was calculating the correction to the OD due to
    saturation incorrectly.  The saturation intensity one should use
    is the bare saturation intensity WITHOUT the polarization correction,
    so for Rb87 this is about 16 W/m^2.  Equation 3.16 in my PhD thesis
    is incorrect, although 3.17 and 3.18 are still valid.

--- AtomImageConstants.m
+++ AtomImageConstants.m
@@ -102,7 +102,7 @@ classdef AtomImageConstants < handle
         end
         
         function Nsat = satN(self)
-            Nsat = self.Isat.*(self.pixelSize/self.magnification)^2*self.exposureTime/(const.h*const.c/self.wavelength)/self.photonsPerCount*self.polarizationCorrection*(1+4*(self.detuning/self.gamma).^2);
+            Nsat = self.Isat.*(self.pixelSize/self.magnification)^2*self.exposureTime/(const.h*const.c/self.wavelength)/self.photonsPerCount*(1+4*(self.detuning/self.gamma).^2);
         end
 
     end
