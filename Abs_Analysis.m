bd4e56b5dea1c232f14d82a7e9cfd7c268584fbc
Ryan Thomas <ryan.j.thomas1@gmail.com>
2021-04-01 17:02:35 +1100

    Changes to wrapper files

--- Abs_Analysis.m
+++ Abs_Analysis.m
@@ -1,7 +1,7 @@
 function cloud = Abs_Analysis(varargin)
 
 atomType = 'Rb87';
-tof = 25e-3;
+tof = 35e-3;
 
 col1 = 'b.-';
 col2 = 'r--';
@@ -9,20 +9,20 @@ dispOD = [0,2];
 plotOpt = 0;
 plotROI = 0;
 
-fitdata = AtomCloudFit('roiRow',[10,1800],...
-                       'roiCol',[10,1300],...
-                       'roiStep',10,...
-                       'fittype','gauss2d');    %Options: none, gauss1d, twocomp1d, bec1d, gauss2d, twocomp2d, bec2d
+fitdata = AtomCloudFit('roiRow',[500,800],...
+                       'roiCol',[10,300],...
+                       'roiStep',1,...
+                       'fittype','2comp2d');    %Options: none, gauss1d, twocomp1d, bec1d, gauss2d, twocomp2d, bec2d
 % fitdata = AtomCloudFit('roiRow',[500,750],...
 %                        'roiCol',[10,250],...
 %                        'roiStep',2,...
 %                        'fittype','none'); 
 
-% detFunc = @(x) (x-8.4985)*12.918;
-detFunc = @(x) (x-8.578)*14.8652;
+detFunc = @(x) (x-8.4985)*12.918;
+% detFunc = @(x) (x-8.578)*14.8652;
 imgconsts = AtomImageConstants(atomType,'exposureTime',30e-6,...
             'pixelsize',6.45e-6,'magnification',0.99,...
-            'freqs',2*pi*[40,23,8],'detuning',detFunc(8.5),...
+            'freqs',2*pi*[53,53,25],'detuning',detFunc(8.5),...
             'polarizationcorrection',1.5,'satOD',5);
 
 directory = 'E:\RawImages\2021';
@@ -62,7 +62,7 @@ for jj = 1:numImages
     cloud(jj).makeImage;
 %     cloud(jj).fitdata.makeFitObjects(cloud(jj).x,cloud(jj).y,cloud(jj).image);
     cloud(jj).fit([],tof,'y');
-%     cloud(jj).fit([],tof,'y',2);
+%     cloud(jj).fit([],tof,'y',3);
         
     %% Plotting
     if plotOpt
